f434a49f3bca5aeab53dcb5d400cf2a78e5c1987
christykusuma <christykusuma96@gmail.com>
2017-10-31 14:01:50 -0400

    references

--- db/migrate/20171031161310_create_assignments.rb
+++ db/migrate/20171031161310_create_assignments.rb
@@ -2,6 +2,8 @@ class CreateAssignments < ActiveRecord::Migration[5.1]
   def change
     create_table :assignments do |t|
 
+    t.references :boat
+    t.references :job
       t.timestamps
     end
   end
