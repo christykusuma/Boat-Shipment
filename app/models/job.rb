1c3a6ff43be2d6af7168795f50a3abebbd56ac0c
Mahima Tuladhar <mahima.k.tuladhar@gmail.com>
2017-11-01 12:19:13 -0400

    updated models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,7 @@
 class Job < ApplicationRecord
 	has_many :assignments
 	has_many :boats, through: :assignments
+	validates :description, length: {minimum:50}
+	validates :title, uniqueness:true
+	validates :amount, numericality:true
 end
