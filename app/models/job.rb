6a612b812add24cd735387f513e10b139ff14795
Mahima Tuladhar <mahima.k.tuladhar@gmail.com>
2017-11-01 13:57:33 -0400

    validation changes

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,5 @@ class Job < ApplicationRecord
 	has_many :boats, through: :assignments
 	validates :description, length: {minimum:50}
 	validates :title, uniqueness:true
-	validates :amount, numericality:true
+	validates :amount, numericality: {greater_than_or_equal_to: 1000}
 end
